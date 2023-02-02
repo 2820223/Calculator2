3c0ccd62b816e0c1b655ee7cd2218df4902bca76
own <missbahodirova@gmail.com>
2023-02-02 14:22:40 +0500

    first

--- app/src/main/java/uz/itschool/calculator/MainActivity.kt
+++ app/src/main/java/uz/itschool/calculator/MainActivity.kt
@@ -1,11 +1,168 @@
 package uz.itschool.calculator
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.math.min
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var one: Button
+    private lateinit var two: Button
+    private lateinit var three: Button
+    private lateinit var four: Button
+    private lateinit var five: Button
+    private lateinit var six: Button
+    private lateinit var seven: Button
+    private lateinit var eight: Button
+    private lateinit var nine: Button
+    private lateinit var zero: Button
+    private lateinit var point: Button
+    private lateinit var c: Button
+    private lateinit var delete_last: Button
+    private lateinit var kopaytiruv: Button
+    private lateinit var boluv: Button
+    private lateinit var foiz: Button
+    private lateinit var qavs_minus: Button
+    private lateinit var minus: Button
+    private lateinit var plus: Button
+    private lateinit var teng: Button
+    private lateinit var operand: TextView
+    private lateinit var result: TextView
+
+    private var ispoint = true
+    private var isdigit = false
+
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        six = findViewById(R.id.six)
+        one = findViewById(R.id.one)
+        two = findViewById(R.id.two)
+        three = findViewById(R.id.three)
+        foiz = findViewById(R.id.foiz)
+        four = findViewById(R.id.four)
+        five = findViewById(R.id.five)
+        seven = findViewById(R.id.seven)
+        eight = findViewById(R.id.eight)
+        nine = findViewById(R.id.nine)
+        zero = findViewById(R.id.zero)
+        point = findViewById(R.id.point)
+        boluv = findViewById(R.id.boluv)
+        kopaytiruv = findViewById(R.id.kopaytiruv)
+        qavs_minus = findViewById(R.id.qavs_minus)
+        c = findViewById(R.id.c)
+        delete_last = findViewById(R.id.delete_last)
+        plus = findViewById(R.id.plus)
+        minus = findViewById(R.id.minus)
+        teng = findViewById(R.id.teng)
+        operand = findViewById(R.id.operand)
+        result = findViewById(R.id.result)
+
+        one.setOnClickListener(this)
+        two.setOnClickListener(this)
+        three.setOnClickListener(this)
+        four.setOnClickListener(this)
+        five.setOnClickListener(this)
+        six.setOnClickListener(this)
+        seven.setOnClickListener(this)
+        eight.setOnClickListener(this)
+        nine.setOnClickListener(this)
+        zero.setOnClickListener(this)
+        point.setOnClickListener { addpoint() }
+
+        plus.setOnClickListener { addSimvol(plus.text.toString()) }
+        minus.setOnClickListener { addSimvol(minus.text.toString()) }
+        kopaytiruv.setOnClickListener { addSimvol(kopaytiruv.text.toString()) }
+        boluv.setOnClickListener { addSimvol(boluv.text.toString()) }
+        qavs_minus.setOnClickListener {manfiySon(operand.text.toString()) }
+
+
     }
-}
+
+
+    @SuppressLint("SetTextI18n")
+    override fun onClick(view: View?) {
+        val btn = findViewById<Button>(view!!.id)
+
+        if (operand.text != "0") {
+            operand.text = operand.text.toString() + btn.text
+
+            isdigit = true
+            ispoint = true
+        } else {
+            operand.text = btn.text
+            ispoint = true
+            isdigit = true
+
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun addpoint() {
+        if (isdigit) {
+            operand.text = operand.text.toString() + point.text
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun addSimvol(simvol: String) {
+        if (isdigit) {
+            operand.text = operand.text.toString() + simvol
+            isdigit = false
+
+        } else {
+            if (operand.text != "0") {
+                operand.text = operand.text.dropLast(1)
+                    .toString() + simvol
+                isdigit = false
+            }
+        }
+        ispoint = true
+    }
+
+    fun manfiySon(s: String) {
+        val list = mutableListOf<Any>()
+        var temp = ""
+        for (i in s) {
+            if (i.isDigit() || i.equals(".")) {
+                temp += i
+            } else {
+                list.add(temp)
+                list.add(i)
+                temp = ""
+            }
+        }
+        if (temp.isNotEmpty()) {
+            list.add(temp)
+        }
+
+
+
+            if (isdigit) {
+                var count = 0
+
+                for ( i in list[list.size-1].toString()){
+                    count++
+                }
+                var lastchar = "hgh"
+                for (i in count ..1){
+
+                     lastchar  = lastchar + operand.text[operand.text.length-i-1].toString()
+                }
+
+                operand.text = operand.text.dropLast(count)
+                operand.text = lastchar
+
+            }
+
+        }
+
+    }
+
